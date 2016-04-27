e249dc15991b3c557d34e739899fdbf5f94795db
plkelley <plkelley@email.arizona.edu>
2016-04-26 21:51:51 -0700

    filled in Timer setting PR value for 1second delay

--- timer.c
+++ timer.c
@@ -1,7 +1,7 @@
 /* 
  * File:   timer.c
- * Author: gvanhoy
- *
+ * Author: gvanhoy/kelley
+ * Kelley: filled in the TODO portions
  * Created on August 27, 2015, 3:26 PM
  */
 
@@ -10,4 +10,12 @@
 void initTimer1(){
     //TODO: Initialize Timer 1 to have a period of
     // 1 second. Enable its interrupt
+    TMR1 = 0;            //init TMR1 at 0
+    PR1 = 1221*2;        //init PR1
+    T1CONbits.TCKPS = 3; //pre-scalar
+    IEC0bits.T1IE = 1;   //interrupt
+    IFS0bits.T1IF = 0;   //put flag down
+    IPC1bits.T1IP = 3;   //Interrupt Priority
+    T1CONbits.TCS = 0;   //oscillartor
+    T1CONbits.TON = 1;   //Timer on
 }
