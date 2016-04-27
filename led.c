a8e59487cdc7720e4fa6f34aed5f166b19001364
plkelley <plkelley@email.arizona.edu>
2016-04-26 21:43:15 -0700

    filled in LED.c portion

--- led.c
+++ led.c
@@ -1,21 +1,38 @@
 /* 
  * File:   led.c
- * Author: gvanhoy
- *
+ * Author: gvanhoy/kelley
+ * kelley: filled in the TODO portions
  * Created on August 27, 2015, 3:15 PM
  */
 
 #include <xc.h>
 #include "led.h"
 
+#define ON 1
+#define OFF 0
+
+
 void initLEDs(){
-    
-    //TODO: Initialize LEDs
+    //TODO: Initialize LEDs to off
+    TRISDbits.TRISD0 = OFF;
+    TRISDbits.TRISD1 = OFF;
+    TRISDbits.TRISD2 = OFF;
 }
 
 void turnOnLED(int led){
-    
-    //TODO: You may choose to write this function
-    // as a matter of convenience
-    
+    if(led == 1){
+        LATDbits.LATD0 = ON;
+        LATDbits.LATD0 = OFF;
+        LATDbits.LATD0 = OFF;
+    }
+    else if(led == 2){
+        LATDbits.LATD0 = OFF;
+        LATDbits.LATD0 = ON;
+        LATDbits.LATD0 = OFF;
+    }
+    else{
+        LATDbits.LATD0 = OFF;
+        LATDbits.LATD0 = OFF;
+        LATDbits.LATD0 = ON;
+    }
 }
