5e738de3e00f3c8984a8059ae07cec5db9b29a08
plkelley <plkelley@email.arizona.edu>
2016-05-02 21:31:49 -0700

    not afraid of commitment

--- timer.c
+++ timer.c
@@ -11,11 +11,24 @@ void initTimer1(){
     //TODO: Initialize Timer 1 to have a period of
     // 1 second. Enable its interrupt
     TMR1 = 0;            //init TMR1 at 0
-    PR1 = 1221*2;        //init PR1
+    PR1 = 39062;         //init PR1
     T1CONbits.TCKPS = 3; //pre-scalar
     IEC0bits.T1IE = 1;   //interrupt
     IFS0bits.T1IF = 0;   //put flag down
-    IPC1bits.T1IP = 3;   //Interrupt Priority
+    IPC1bits.T1IP = 7;   //Interrupt Priority
     T1CONbits.TCS = 0;   //oscillartor
     T1CONbits.TON = 1;   //Timer on
+}
+/**/
+initTimer2(){ //debouncing
+    TMR2 = 0;  //init TMR2 at 0
+    T2CONbits.TCKPS = 0;
+    T2CONbits.TCKPS = 0;
+    IFS0bits.T2IF = 0;
+}
+
+delayMS(unsigned int delay){
+    TMR2 = 0;
+    PR2 = delay*624;
+    
 }
